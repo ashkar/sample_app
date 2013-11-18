d9b3d0b36ba66a0a610946642506f6bcd5ee43c5
ashkar <ashkar55555@gmail.com>
2013-11-18 16:29:34 +0530

    smtp settings

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -54,6 +54,20 @@ SampleApp::Application.configure do
   # Enable threaded mode
   # config.threadsafe!
 
+  config.action_mailer.default_url_options = { :host => 'fierce-journey-9260.herokuapp.com' }
+  config.action_mailer.delivery_method = :smtp
+  config.action_mailer.perform_deliveries = true
+  config.action_mailer.raise_delivery_errors = false
+  config.action_mailer.default :charset => "utf-8"
+  config.action_mailer.smtp_settings = {
+  address: "smtp.gmail.com",
+  port: 587,
+  domain: "fierce-journey-9260.herokuapp.com",
+  authentication: "plain",
+  enable_starttls_auto: true,
+  user_name: ENV["GMAIL_USERNAME"],
+  password: ENV["GMAIL_PASSWORD"]
+}
   # Enable locale fallbacks for I18n (makes lookups for any locale fall back to
   # the I18n.default_locale when a translation can not be found)
   config.i18n.fallbacks = true
